0314e8fbf6b7cdc6ba3b990d43605b6a5dbaec1b
Leo Crapart <leocrapart@yahoo.fr>
2023-01-05 01:26:26 +0100

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to iad-scraping
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/iad_scraping/core.clj
@@ -0,0 +1,7 @@
+(ns iad-scraping.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
